af212edb9b2aedec0a96763ecf0dc6679cfab0f5
thinh-shito <dothinhtpr247@gmail.com>
2022-03-24 19:40:40 +0700

    demo 1

--- visualization.py
+++ visualization.py
@@ -1,10 +1,130 @@
 from tkinter import *
+import redblacktree as rbt
+from tkinter import *
 from tkinter import Canvas
+import time
+import redblacktree as rbt
+
+window_width = 1680
+window_height = 1050
+
+canvas_width = 1600
+canvas_height = 970
+
+mid_canvas_width = canvas_width // 2
+mid_canvas_height = canvas_height // 2
+
+f = ('Roboto', 16, 'bold')
+
+colors = ['black', 'red']
+radius = 20
+
+day = "day"
+nodes = []
+
+
+
+
+
+
+
+def insert_tree():
+    c.delete(day)
+    ar_split = box.get().split(',')
+    box.delete(0, END)
+    for i in ar_split:
+        rbt.insert(int(i))
+        # rbt.print_tree()
+
+        visualize()
+
+
+def delete_tree():
+    c.delete(day)
+    ar_split = box.get().split(',')
+    box.delete(0, END)
+    for i in ar_split:
+        nodes.remove([f'o{i}', f'l{i}'])
+        rbt.delete_node(int(i))
+        c.delete(ALL)
+        visualize()
+
+    print(ar_split)
+
+def find_tree():
+    c.delete(day)
+    ar_split = box.get().split(',')
+    box.delete(0, END)
+
+
+def emtpy_canvas():
+    c.delete(ALL)
+    circles.clear()
+
+
+def list_node():
+    s = ''.join(str(i) + ' ' for i in rbt.get_list_node())
+    return s
+
+
+def print_tree():
+    s = list_node()
+    # c.delete(tags[0])
+    node = c.create_text(mid_canvas_width, canvas_height - 50,
+                         text=s, font=f, fill='black', tag=day, justify='center')
+
+
+def visualize():
+    tree = rbt.root  # start with root of the tree
+    # Place root node at position tree.pos
+    if tree.item != 0:
+        tree.pos = [mid_canvas_width, 100]
+        create_node(tree.pos, tree.color, f"{tree.item}")
+
+        # Recursively place the other nodes and edges
+        level = 0
+
+        def add_nodes(tree, level):
+            if tree.left.item != 0:  # if left subtree: position node to left of parent
+                tree.left.pos[0] = tree.pos[0] - 12 * radius // level  # x
+                tree.left.pos[1] = tree.pos[1] + 2 * radius  # y
+                create_node(tree.left.pos, tree.left.color, f"{tree.left.item}")
+                add_nodes(tree.left, level + 1)
+            if tree.right.item != 0:  # if right subtree: position node to right of parent
+                tree.right.pos[0] = tree.pos[0] + 12 * radius // level  # x
+                tree.right.pos[1] = tree.pos[1] + 2 * radius  # y
+                create_node(tree.right.pos, tree.right.color, f"{tree.right.item}")
+                add_nodes(tree.right, level + 1)
+
+        if tree.left or tree.right:
+            add_nodes(tree, level + 1)
+
+
+# def create_node(x,y, r = radius, color='black', text='null'):
+#     new_circle = c.create_oval(x-r, y-r, x+r, y+r, fill=color, outline='black', width=2)
+#     new_txt = c.create_text(x, y, text=text, font= f, fill='white', justify='center')
+#     circles.append([new_circle, new_txt])
+
+def create_node(pos, color=0, text='null'):
+    x = pos[0]
+    y = pos[1]
+    r = radius
+    cor = x - r, y - r, x + r, y + r
+    o_tag = 'o'+text
+    l_tag = 'l'+text
+    c.create_oval(cor, fill=colors[color], outline='black', width=2, tag=o_tag)
+    c.create_text(pos, text=text, font=f, fill='white',justify='center', tag=l_tag)
+    nodes.append([o_tag, l_tag])
+
+
+def create_edge(x0, y0, x, y, color='black'):
+    c.create_line(x0, y0, x, y, fill=color, width=3)
+
+
+rbt = rbt.RedBlackTree()
 
 root = Tk()
-root.title("red black tree")
-window_width = 1500
-window_height = 900
+root.title("Red Black Tree Visualization")
 
 # get the screen dimension
 screen_width = root.winfo_screenwidth()
@@ -16,58 +136,42 @@ center_y = int(screen_height / 2 - window_height / 2)
 
 # set the position of the window to the center of the screen
 root.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
-# root.geometry("1500x1000+50+50")
-root.resizable(0, 0)
+
+# create canvas
+
+c = Canvas(root, width=canvas_width, height=canvas_height, bg='white')
 
 
-def split_string():
-    ar_split = insertion_textbox.get().split(',')
-    insertion_textbox.delete(0, END)
-    print(ar_split[1])
-    return ar_split
+c.pack(padx=40, pady=40, fill=BOTH, expand=True)
 
 
-# add insert button
-insert_button = Button(root, text="Insert", command=split_string)
-insert_button.grid(column=1, row=0, padx=3, pady=3)
+# add insertion textbox
+box = Entry(root, width=20, font=('Roboto', 16))
+box.place(x=40, y=10)
 
-insertion_textbox = Entry(root)
-insertion_textbox.grid(column=0, row=0, padx=3, pady=3)
-
-# add delete button
-delete_button = Button(root, text="Delete", command=split_string)
-delete_button.grid(column=1, row=1, padx=3, pady=5)
-
-delete_textbox = Entry(root)
-delete_textbox.grid(column=0, row=1, padx=3, pady=5)
+# add insertion button
+insert_button = Button(root, text="Insert", command=insert_tree)
+insert_button.place(x=260, y=8)
 
 # add find button
-find_button = Button(root, text="Find")
-find_button.grid(column=1, row=2, padx=3, pady=5)
+search_button = Button(root, text="Search", command=find_tree)
+search_button.place(x=335, y=8)
 
-find_textbox = Entry(root)
-find_textbox.grid(column=0, row=2, padx=3, pady=5)
+# add deletion button
+deletion_button = Button(root, text="Delete", command=delete_tree)
+deletion_button.place(x=420, y=8)
 
-checkbox1 = Checkbutton(root,
-                        text='pre order',
-                        onvalue='agree',
-                        offvalue='disagree')
-checkbox1.place(x=20, y=110)
-checkbox2 = Checkbutton(root,
-                        text='post order',
-                        onvalue='agree',
-                        offvalue='disagree')
-checkbox2.place(x=20, y=150)
-checkbox3 = Checkbutton(root,
-                        text='in order',
-                        onvalue='agree',
-                        offvalue='disagree')
-checkbox3.place(x=20, y=190)
+# add  empty button
+empty_button = Button(root, text="Empty", command=emtpy_canvas)
+empty_button.place(x=500, y=8)
 
-# add find button
-print_button = Button(root, text="Print").place(x=120, y=150)
+# add print button
+print_button = Button(root, text="Print", command=print_tree)
+print_button.place(x=580, y=8)
 
-quit_button = Button(root, text="Quit", command=root.destroy).place(y=860, x=10)
+# # add exit button
+# quit_button = Button(
+#     root, text="Quit", command=root.destroy).place(y=860, x=10)
+print(list_node())
 
-c = Canvas(root, width=1180, height=890, bg='white').place(x=310, y=0)
 root.mainloop()
